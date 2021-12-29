ee9de6c1b63aeaefe401e29eacd678d5cec532d1
Palatin21 <90023720+Palatin21@users.noreply.github.com>
2021-12-29 09:05:17 +0300

    Update prog1.c

--- prog1.c
+++ prog1.c
@@ -3,40 +3,41 @@
 #include <time.h>
 
 
-int main()
-{
-int i;
-int maximum, minimum;
-int size;
-const long double RAND_MAX_F = RAND_MAX;
 
-//Диалоговые строки
-printf("Сколько элементов должен содержать массив ");
-scanf("%d",&size);
-printf("Укажите минимальное значение массива ");
-scanf("%d", &minimum);
-printf("Укажите максимальное значение массива ");
-scanf("%d", &maximum);
+int main123() {
 
-//Пользовательские ошибки
-if (minimum > maximum) {
-printf("Минимальное значение не может быть выше минимального");
-return 0;
-}
+  int i;
+  int maximum, minimum;
+  int size;
+  const long double RAND_MAX_F = RAND_MAX;
 
-if (size <= 0 ) {
-printf("Генерируемый массив не может быть пустым");
-return 0;
-}
+  //Диалоговые строки
+  printf("Сколько элементов должен содержать массив ");
+  scanf("%d",&size);
+  printf("Укажите минимальное значение массива ");
+  scanf("%d", &minimum);
+  printf("Укажите максимальное значение массива ");
+  scanf("%d", &maximum);
 
-//Создание файла
-srand(time(NULL));
-FILE* file = 0;
-file = fopen("/Users/prog/textFile.txt", "w");
-for(i=0; i<size; i++){
-fprintf(file, "%Lf\n", (rand() / RAND_MAX_F) * ((double)maximum - (double)minimum) + maximum);
-}
-fclose(file);
+  //Пользовательские ошибки
+  if (minimum > maximum) {
+  printf("Минимальное значение не может быть выше минимального");
+  return 0;
+  }
 
-return 0;
+  if (size <= 0 ) {
+  printf("Генерируемый массив не может быть пустым");
+  return 0;
+  }
+
+  //Создание файла
+  srand(time(NULL));
+  FILE* file = 0;
+  file = fopen("textFile.txt", "w");
+  for(i=0; i<size; i++){
+  fprintf(file, "%Lf\n", ((maximum - minimum) * ((long double)rand() / RAND_MAX)) + minimum);
+  }
+  fclose(file);
+
+  return 0;
 }
